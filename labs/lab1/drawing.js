57d20df3d0ee563918438bd682358fb73176e045
BrittanyMSams <samsbrittany2000@gmail.com>
2020-09-04 18:15:35 -0400

    Lab1 done

---
+++ labs/lab1/drawing.js
@@ -0,0 +1,44 @@
+function setup() {
+    createCanvas(800,600);
+
+    var white = "#f7f7f7";
+    var black = "#2b2b2b";
+    var purple = "#faeeff"
+    noStroke();
+
+    fill(white);
+    rect(5,5,600,600);
+
+    fill(black);
+    circle(300,300,260);
+    fill(white);
+    circle(300,300,220);
+    fill(black);
+    circle(300,300,180);
+    fill(white);
+    circle(300,300,140);
+    fill(black);
+    circle(300,300,100);
+    fill(white);
+    circle(300,300,60);
+    fill(black);
+    circle(300,300,20);
+
+    fill(white);
+    rect(545,0,30,600);
+    fill(black);
+    rect(460,0,30,600);
+    fill(white);
+    rect(370,0,30,600);
+    fill(black);
+    rect(284,0,30,600);
+    fill(white);
+    rect(198,0,30,600);
+    fill(black);
+    rect(110,0,30,600);
+    fill(white);
+    rect(30,0,30,600);
+
+    fill(purple)
+    circle(300,300,5)
+}
