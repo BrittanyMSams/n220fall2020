69873b73fc3ee77f7d7bf4e2a321629b2e90d8c6
BrittanyMSams <samsbrittany2000@gmail.com>
2020-09-11 18:12:38 -0400

    Update evenlarger.js

--- labs/lab 2/evenlarger/evenlarger.js
+++ labs/lab 2/evenlarger/evenlarger.js
@@ -1,18 +1,22 @@
-function draw(){
+let D = 1;
+
+function setup(){
     createCanvas (800,600);
+}
+
+function draw(){
     background ("#87ffd3");
     noStroke();
 
-    let D = 40;
-
-    D++;
+    D += 3;
 
     fill("#59383d");
     circle (400, 300, D);
 
-    if(D > 300){
-        fill("#59383d");
+    if(D > 200){
         circle (400, 300, D);
+        D = 1;
     }
 
 }
+
